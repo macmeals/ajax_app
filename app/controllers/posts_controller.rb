d9d9f6fba235d8335da81feeb364fd938bd494a2
macmeals <loseworld9731@gmail.com>
2020-09-07 23:54:21 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,27 @@
 class PostsController < ApplicationController
   def index  # indexアクションを定義した
     @posts = Post.all.order(id: "DESC") # 全てのレコードを＠postsへ取得する
-    
   end
+
    #def new
     
    #end
+
   def create
     Post.create(contents: params[:content])
     redirect_to action: :index
   end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked 
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
+
 end
